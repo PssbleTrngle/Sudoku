247d9669ab372c11fb34c71e14ba48d109a91647
PssbleTrngle <nik@somethingcatchy.net>
2021-04-25 20:35:20 +0200

    make 'fill candidates' a button instead of a checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,10 @@ function App() {
             .catch(e => console.error(e.message))
     }
 
+    useEffect(() => {
+        if(fillCanditates) setFillCandidates(false)
+    }, [fillCanditates])
+
     return <>
 
         <div className='toolbar'>
@@ -72,15 +76,20 @@ function App() {
                 <button key={k} onClick={generating ? cancel : call}>{generating ? 'Cancel' : k}</button>
             )}
 
+            {/*}
             <label htmlFor='fill-candidates'>Fill Candidates</label>
             <input type='checkbox' id='fill-candidates' checked={fillCanditates} onChange={e => setFillCandidates(e.target.checked)} />
+            */}
+
+
+            <button onClick={() => setFillCandidates(true)}>Fill Candidates</button>
 
         </div>
 
         <Sudoku fillCanditates={fillCanditates} sudoku={sudoku} onChange={s => setSudoku(o => {
-            const set = typeof s === 'function' ? s : () => s
-            return set(o)
-        })} />
+                const set = typeof s === 'function' ? s : () => s
+                return set(o)
+            })} />
     </>
 }
 
--- src/logic/strategies/NakedPair.ts
+++ src/logic/strategies/NakedPair.ts
@@ -23,23 +23,28 @@ export default class NakedPair extends Strategy {
          const partners = blockers.filter(c => arrayEqual(c.possibles, possibles))
 
          if (partners.length !== 1) return null
+         const [partner] = partners
+         const partnerBlockers = possibleBlockers(partner.point.x, partner.point.y, this.sudoku)
 
-         return blockers.map(b =>
-            b.possibles
-               .filter(i => possibles.includes(i))
-               .map<Hint>(value => ({
-                  type: 'exclude',
-                  col: b.point.y,
-                  row: b.point.x,
-                  value,
-                  highlights: [
-                     cell, {
-                        row: partners[0].point.x,
-                        col: partners[0].point.y,
-                     }
-                  ]
-               }))
-         )
+         return blockers
+         .filter(c => !arrayEqual(c.possibles, possibles))
+            .filter(c => partnerBlockers.some(b => b.point.x === c.point.x && b.point.y === c.point.y))
+            .map(b =>
+               b.possibles
+                  .filter(i => possibles.includes(i))
+                  .map<Hint>(value => ({
+                     type: 'exclude',
+                     col: b.point.y,
+                     row: b.point.x,
+                     value,
+                     highlights: [
+                        cell, {
+                           row: partner.point.x,
+                           col: partner.point.y,
+                        }
+                     ]
+                  }))
+            )
 
       }).filter(exists).flat(2)
 
