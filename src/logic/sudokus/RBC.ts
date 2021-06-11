81f03371a55ef6099e1dd8cc977031f7efcd8098
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-11 16:35:18 +0200

    Update RBC.ts

--- src/logic/sudokus/RBC.ts
+++ src/logic/sudokus/RBC.ts
@@ -11,7 +11,9 @@ define('RBC', [
    [[2, 3, 4, 5, 6], [4, 5], 7, [5, 6], 1, 8, [2, 3, 5, 6], 9, [2, 4]],
    [[3, 4, 5, 6, 8, 9], [4, 5, 8, 9], [1, 3, 4, 8], 2, [6, 7, 9], [5, 7, 9], [3, 5, 6, 8], [3, 4, 5, 6], [1, 4, 8]],
    [[2, 5, 6, 8, 9], [5, 8, 9], [1, 8], [5, 6], 4, 3, [2, 5, 6, 8], 7, [1, 2, 8]]
-], RBC, `Test description`)
+], RBC, `Um einen Reihe-Block-Check zu entdecken, müssen Sie nach einer Reihe (Spalte oder Zeile) suchen, in welcher alle Kandidaten eines Wertes in einem Block vorkommen. Zusätzlich müssen innerhalb des Blocks weitere Kandidaten des jeweiligen Wertes vorkommen. Im Beispiel Sudoku kommen in Zeile 4 alle Kandidaten für eine 9 nur innerhalb des mittleren Blocks vor. Die entsprechende Zeile und der Block sind hellblau eingefärbt. Außerdem befinden sich im mittleren Block weitere 9er Kandidaten.
+Da die beiden 9er in der vierten Zeile die einzigen in dieser Reihe sind, muss einer von beiden der richtige sein. Daher sind anderen die 9er Kandidaten innerhalb des Blocks auf jeden Fall falsch und können somit gestrichen werden. Deshalb sind diese rot markiert.
+`)
 
 define('RBC 2', [
    [[3, 6, 9], [3, 6, 9], 2, [5, 6, 9], [5, 6, 7, 9], 1, [3, 5, 7], 4, 8],
