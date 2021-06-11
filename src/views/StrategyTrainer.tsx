90cdd5afaca820e7e9fb65cf021e9e0f1506d18a
PssbleTrngle <nik@somethingcatchy.net>
2021-06-11 16:25:26 +0200

    Trainer view

--- src/views/StrategyTrainer.tsx
+++ src/views/StrategyTrainer.tsx
@@ -1,34 +1,77 @@
 import { FC, useMemo } from 'react'
-import { Redirect, Route, useParams, useRouteMatch } from 'react-router-dom'
+import { Redirect, useParams, useRouteMatch } from 'react-router-dom'
+import styled from 'styled-components'
 import { RouterLinkButton as Link } from '../components/Inputs'
-import { getSudokus } from '../logic/sudokus'
-import SelectedSudoku from './SelectedSudoku'
-import { List } from './Trainer'
+import Sudoku from '../components/Sudoku'
+import { getSudokus, SudokuInfo } from '../logic/sudokus'
 
 const StrategyTrainer: FC = () => {
    const { url } = useRouteMatch()
    const { slug } = useParams<Record<string, string>>()
 
-   const sudokus = useMemo(() => getSudokus().filter(s => s.strategy?.slug === slug), [slug])
-   const strategy = useMemo(() => sudokus[0]?.strategy, [sudokus])
+   const [example, ...rest] = useMemo(() => getSudokus()
+      .filter(s => s.strategy?.slug === slug)
+      .sort(it => it.description ? -1 : 1),
+      [slug]
+   )
+
+   const strategy = useMemo(() => example?.strategy, [example])
 
    if (!strategy) return <Redirect to='/trainer' />
 
+   return <Style>
+      <Example {...example} />
+      {rest.length > 0 && <Link to={`/creator?load=${rest[0].slug}`}>Selbst Versuchen</Link>}
+   </Style>
+}
+
+const Example: FC<SudokuInfo> = ({ strategy, sudoku, description }) => {
+
+   const [hint] = useMemo(() => strategy!.create(sudoku).getHints() ?? [], [strategy, sudoku])
+
    return (
       <>
-         <Route path={`${url}/:slug`} component={SelectedSudoku} />
+         <Sudoku {...sudoku} hint={hint} />
 
-         <Route path={url} exact>
-            <List>
-               {sudokus.map(sudoku => (
-                  <Link key={sudoku.name} to={`/trainer/${slug}/${sudoku.slug}`}>
-                     <li>{sudoku.name}</li>
-                  </Link>
-               ))}
-            </List>
-         </Route>
+         <h2>{strategy!.name}</h2>
+
+         <p>{description || 'No description provided'}</p>
       </>
    )
+
 }
+
+const Style = styled.section`
+   display: grid;
+   justify-content: center;
+   padding: 2rem;
+   gap: 2rem;
+   margin: 0 auto;
+   width: min-content;
+   text-align: center;
+
+   h2 {
+      grid-area: strategy;
+   }
+
+   p {
+      grid-area: description;
+   }
+
+   a {
+      grid-area: button;
+      height: max-content;
+      width: max-content;
+      margin: 0 auto;
+   }
+
+   grid-template:
+      'sudoku .' 200px
+      'sudoku strategy'
+      'sudoku description'
+      'sudoku button'
+      'sudoku .'
+      / 1fr 300px;
+`
 
 export default StrategyTrainer
