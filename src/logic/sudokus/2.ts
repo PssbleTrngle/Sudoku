308737a7e4f74e328421447bce3a905a35a0eb0a
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-12 14:14:09 +0200

    Update 2.ts

--- src/logic/sudokus/2.ts
+++ src/logic/sudokus/2.ts
@@ -10,4 +10,4 @@ define('Nackter Single', [
    [2, 0, 5, 0, 0, 6, 4, 0, 0],
    [9, 0, 1, 0, 0, 7, 0, 0, 0],
    [7, 0, 8, 0, 0, 2, 0, 0, 0],
-])
+], Nackter Single, 'Ein nackter Single liegt vor, wenn in ein Feld nur noch eine Zahl eingetragen werden kann, da es im Einflussbereich von acht Zahlen liegt. Um einen nackten Single zu ermitteln, müssen Sie die freien Felder einzeln untersuchen. Suchen Sie nach einem freien Feld, in dessen Zeile, Spalte und Block insgesamt 8 verschiedene Zahlenwerte vorkommen. In unserem Beispiel ist dies im ersten Feld der ersten Zeile der Fall. Wie Sie durch die blauen Umrandungen nachverfolgen können, finden Sie im Einflussbereich unseres Feldes alle Zahlenwerte außer der 8. Dies ist der fehlende Zahlenwert und Sie können diesen in das Feld eintragen. In unserem Beispiel haben wir Ihnen das mit einer grünen 8 dargestellt. ')
