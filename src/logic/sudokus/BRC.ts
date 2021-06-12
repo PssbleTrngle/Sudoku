16eb361747aef386774b09b618072d7224c13804
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-12 13:07:07 +0200

    Update BRC.ts

--- src/logic/sudokus/BRC.ts
+++ src/logic/sudokus/BRC.ts
@@ -10,9 +10,9 @@ define('BRC', [
    [8, 0, 0, 0, 7, 0, 0, 0, 0],
    [0, 0, 0, 6, 0, 0, 4, 0, 0],
    [0, 0, 0, 3, 0, 0, 0, 0, 9],
-])
+], BRC, 'In Abbildung 14 ist der betreffende Kandidatenwert die 9. Dieser kommt in der vierten Zeile nur im mittleren Block vor. Diese Felder sind daher blau umrandet. Da die restlichen 9er im mittleren Block gestrichen werden können sind diese in Abbildung 14 rot markiert.')
 
-define('BRC', [
+define('BRC 2', [
             [4, [3,5,7,8,9], [3,5,7,8], [2,7,9], [1,2,7,9], [2,3,7], [1,3,5,8], [2,3,8], 6],
    [2, [3,6,9], [3,6], 8, 5, [3,6], 4, 7, [1]],
    [[3,6,7], [3,5,6,7,8], 1, [2,6,7], [2,6,7], 4, 9, [2,3,8], [2,5]],
