4eb0db364489607e5e73bd806ec94feb3460d487
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-12 21:17:38 +0200

    Update Swordfish.ts

--- src/logic/sudokus/Swordfish.ts
+++ src/logic/sudokus/Swordfish.ts
@@ -11,4 +11,4 @@ define('Schwertfisch', `
    007  8  5
    00009  6
    1  4  7
-`, Swordfish)
+`, Swordfish, 'Um einen Schwertfisch anzuwenden, müssen Sie nach drei Zeilen suchen, in denen ein Kandidatenwert nur in drei Spalten vorkommt. Im Beispiel sind diese Felder blau umrandet und der vorkommende Kandidatenwert ist die 4. Nun müssen Sie nach weiteren Kandidaten dieses Wertes in den drei Spalten suchen. Diese können Sie streichen, da der Wert in dreien der Schnittfelder eingetragen werden müssen. Im Beispiel sehen Sie, wie die4er in den markierten Spalten gestrichen wurden. Diese Strategie ist auch umgekehrt anwendbar für Spalten mit allen Kandidatenwerten in nur drei Spalten und dem Streichen aus drei Zeilen.')
