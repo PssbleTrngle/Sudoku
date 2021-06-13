67d9c358988d2bb1007069dd88d9e3539361536b
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-13 13:28:42 +0200

    Update HiddenTriple.ts

--- src/logic/sudokus/HiddenTriple.ts
+++ src/logic/sudokus/HiddenTriple.ts
@@ -11,4 +11,4 @@ define('Versteckter Dreier', [
    [[5, 6, 7, 9], [5, 6, 7, 8], [5, 7, 8, 9], 3, [1, 6, 8, 9], [1, 2, 4, 5, 6, 8, 9], [1, 2, 4, 8, 9], [1, 6, 8, 9], [2, 4, 6, 9]],
    [1, 2, 4, [6, 8, 9], 7, [6, 8, 9], [3, 8, 9], 5, [3, 6, 9]],
    [[3, 5, 6, 9], [3, 5, 6, 8], [5, 8, 9], [1, 2, 5, 6, 8, 9], [1, 6, 8, 9], [1, 2, 4, 5, 6, 8, 9], 7, [1, 3, 6, 8, 9], [2, 3, 4, 6, 9]]
-], HiddenTriple)
+], HiddenTriple, 'Um einen versteckten Dreier zu ermitteln, suchen Sie nach drei Kandidaten, die in einer Einheit (Zeile, Spalte, Block) in genau drei Feldern vorkommen. In keinem der anderen Felder der Einheit darf sich einer der drei Kandidaten befinden. Im nebenstehenden Beispiel ist dies in Zeile 5 der Fall. Das blau markierten Felder bestehend aus 4, 5 und 7 kommen in dieser Zeile nur in den blau umrandeten Feldern vor. Da diese Werte ausschließlich in diesen drei Feldern vorkommen, müssen diese auch dort eingetragen werden. Daher können alle anderen Kandidatenwerte in diesen Feldern gestrichen werden. Daher wurden dort die 3, 6 und 9 rot markiert. ')
