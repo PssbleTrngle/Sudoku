0158dad311822c2fc654db0f518eb269b2d2f725
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-13 13:19:39 +0200

    Update ClonedPair.ts

--- src/logic/sudokus/ClonedPair.ts
+++ src/logic/sudokus/ClonedPair.ts
@@ -11,4 +11,4 @@ define('Geklonte Paare', `
    5  612738
    003897465
    876345921
-`, ClonedPair)
+`, ClonedPair, 'Um ein geklontes Paar zu ermitteln, müssen Sie Felder suchen, die dasselbe Kandidatenpaar enthalten. Anschließend müssen Sie diese so zu einer Kette verbinden, dass immer zwei dieser Felder innerhalb einer Einheit sind. Im nebenstehenden Beispiel ist eine solche Kette aus 4er und 9er Paaren eingezeichnet. Nun empfiehlt es sich die Felder der Kette durchzunummerieren. Suchen Sie nach einem Feld, das Kandidaten des Paares enthält, und im Einflussbereich eines geraden und ungeraden Feldes der Kette liegt. Denn das Prinzip dieser Kette besagt, dass aufgrund der Tatsache, dass in allen Feldern der Kette nur dasselbe Kandidatenpaar vorliegt, diese immer abwechselnd entlang der Kette liegen müssen. Daher blockieren ein gerades und ein ungerades Feld der Kette ein Feld in deren Einflussbereich. In unserem Beispiel ist das erste Feld in der sechsten Zeile in der Reihe zweier solcher Felder. Deswegen müssen die 4 und die 9 als Kandidaten gestrichen werden.')
