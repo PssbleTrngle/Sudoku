e9e8de97f12a435ae2d04a1c6e573e1d44ca9d44
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-13 13:31:56 +0200

    Update HiddenQuadruple.ts

--- src/logic/sudokus/HiddenQuadruple.ts
+++ src/logic/sudokus/HiddenQuadruple.ts
@@ -11,4 +11,4 @@ define('Versteckter Vierer', [
    [[1, 3, 4, 5, 7, 8], [4, 8], 9, 2, [3, 5, 8], [1, 3, 4, 5, 8], [4, 5, 7], 6, [4, 7]],
    [[4, 5, 6, 8], [4, 6, 8], [4, 5, 8], [4, 5, 8], 7, 9, 3, 2, 1],
    [[1, 3, 4, 5, 7], [1, 2, 4], [2, 3, 4, 5], [1, 3, 4, 5], [3, 5], 6, [4, 5, 7], 8, 9]
-], HiddenQuadruple)
+], HiddenQuadruple, 'Um einen versteckten Vierer zu ermitteln, suchen Sie nach vier Kandidaten, die in einer Einheit (Zeile, Spalte, Block) in genau vier Feldern vorkommen. In keinem der anderen Felder der Einheit darf sich einer der vier Kandidaten befinden. Im nebenstehenden Beispiel ist dies im linken unteren Block der Fall. Die blau markierten Felder bestehend aus 1, 2, 3 und 7 kommen in diesem Block nur in den blau umrandeten Feldern vor. Da diese Werte ausschließlich in diesen vier Feldern vorkommen, müssen diese auch dort eingetragen werden. Daher können alle anderen Kandidatenwerte in diesen Feldern gestrichen werden. Daher wurden dort die 4, 5 und 8 rot markiert. ')
