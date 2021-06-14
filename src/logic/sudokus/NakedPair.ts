036bc0e28d5ec75a5624af49a787d5f42d11bfcf
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-14 09:48:19 +0200

    Update NakedPair.ts

--- src/logic/sudokus/NakedPair.ts
+++ src/logic/sudokus/NakedPair.ts
@@ -12,3 +12,16 @@ define('Nacktes Paar', [
    [8, 7, 4, [2, 6, 9], [5, 6], [1, 2, 5, 9], [2, 3, 5, 6], [1, 5, 9], [1, 2, 3, 5, 6, 9]],
    [[1, 2, 9], 6, 5, 3, 4, 8, [2, 7], [1, 7, 9], [1, 2, 7, 9]]
 ], NakedPair, 'Ein nacktes Paar liegt vor, wenn sich in einer Einheit (Zeile, Spalte, Block) genau zwei Felder befinden, in denen ausschließlich das gleiche Kandidatenpaar vorkommt. Durchsuchen Sie dazu das Sudoku nach Feldern, in denen nur zwei Kandidaten vorkommen. Haben Sie ein solches Feld gefunden, dann suchen Sie nach einem Feld, das in derselben Einheit liegt und ebenfalls nur dieses Kandidatenpaar enthält. In unserem Beispiel ist dies in der dritten Zeile der Fall. Dort sehen Sie zwei blau umrandete Felder, in denen nur eine 2 und eine 4 vorkommen. Da in eines dieser Felder sicher die 4 und in das andere auf jeden Fall die 2 eingetragen werden muss, können Sie aus allen weiteren Felder dieser Einheit diese Kandidatenwerte streichen. Im Falle unseres Beispiels können die rot markierten 2er und 4er gestrichen werden.')
+
+
+define('Nacktes Paar 2', [
+            [[], 1, [], 6, [], [], [], [], []],
+   [[], [], [], [], [], [], 3, 5, []],
+   [[], 5, [], 4, [], [], [], 2, []],
+   [[], [], [], [], [], [], 9, 1, 2],
+   [[], [], 2, [], 8, [], 6, [], []],
+   [[], [], 7, [], 9, [], [], [], []],
+   [8, [], [], [], 6, [], 2, [], 1],
+   [2, [], [], 8, [], 4, [], [], []],
+   [4, [], [], [], 5, 3, 8, [], 9]
+        ], NakedPair)
