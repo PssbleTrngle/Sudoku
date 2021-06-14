f91ccb7c2511a3fa3f109ad9fa590da63d7329b7
mchriskros <51700583+mchriskros@users.noreply.github.com>
2021-06-14 10:13:14 +0200

    Update NakedQuadruple.ts

--- src/logic/sudokus/NakedQuadruple.ts
+++ src/logic/sudokus/NakedQuadruple.ts
@@ -12,3 +12,16 @@ define('Nackter Vierer', [
    [9, 6, 2, 8, 7, 1, 4, 3, 5],
    [[1, 3], [5, 7, 8], [1, 3, 5, 7], 4, [2, 3], [2, 3, 6], [6, 7, 9], [7, 8, 9], [1, 6]]
 ], NakedQuadruple, 'Ein nackter Vierer liegt vor, wenn sich in einer Einheit (Zeile, Spalte, Block) genau vier Felder befinden, in denen ausschließlich vier Kandidatenwerte vorkommen. Durchsuchen Sie dazu das Sudoku nach vier Feldern einer Einheit, in denen nur vier verschiedene Kandidatenwerte vorkommen. In unserem Beispiel ist dies in der letzten Zeile der Fall. Dort sehen Sie vier blau umrandete Felder, in denen nur 1er, 2er, 3er und 6er vorkommen. Da in eines dieser Felder sicher die 1, in eines sicher die 2, in eines sicher die 3 und in das letzte auf jeden Fall die 6 eingetragen werden muss, können Sie aus allen weiteren Felder dieser Einheit diese Kandidatenwerte streichen. Im Falle unseres Beispiels können die rot markierte 1, 3 und 6 gestrichen werden.')
+
+
+define('Nackter Vierer 2', [
+            [2, 1, [], [], [], [], 4, [], []],
+   [[], [], [], [], 2, 8, [], [], []],
+   [[], [], [], [], [], [], 1, [], 6],
+   [[], [], [], 5, [], 7, 6, [], 8],
+   [8, 3, [], [], [], [], [], [], 7],
+   [[], [], [], [], 1, 6, [], [], 3],
+   [[], 4, 2, 3, [], [], [], [], []],
+   [[], 5, 3, [], [], [], [], 7, []],
+   [[], [], 7, 9, [], [], [], [], []]
+        ], NakedQuadruple)
